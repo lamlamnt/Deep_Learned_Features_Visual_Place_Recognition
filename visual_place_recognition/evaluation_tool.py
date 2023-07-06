a7311c9f6dc08a9e0409a2e83e969f8a95e734de
Lam Lam <lamlam@eng.ox.ac.uk>
2023-07-06 18:06:39 +0100

    Added evaluation tools

---
+++ visual_place_recognition/evaluation_tool.py
@@ -0,0 +1,74 @@
+import numpy as np
+from haversine import haversine
+import matplotlib.pyplot as plt
+
+#Get GPS ground truth between 2 runs
+def gps_ground_truth(reference_run, query_run, reference_len, query_len):
+    #The smaller number of frames of the two runs is used
+    #Vertical axis is reference run. Horizontal axis is query run.
+    ref_name = ""
+    if(reference_run < 10):
+        ref_name = "0" + str(reference_run)
+    else:
+        ref_name = str(reference_run)
+        ref_name = ""
+    que_name = ""
+    if(query_run < 10):
+        que_name = "0" + str(query_run)
+    else:
+        que_name = str(query_run)
+    reference_path ="/Volumes/oridatastore09/ThirdPartyData/utias/inthedark/run_0000" + ref_name + "/gps.txt"
+    query_path = "/Volumes/oridatastore09/ThirdPartyData/utias/inthedark/run_0000" + que_name + "/gps.txt"
+    gps_distance = np.zeros((reference_len, query_len),dtype=float)
+    ref_gps = np.zeros((reference_len,3),dtype=float)
+    query_gps = np.zeros((query_len,3),dtype=float)
+    
+    #Get gps data
+    with open(reference_path, "r") as file:
+        for i in range(reference_len):
+            line = file.readline().strip()  # Read the first line and remove leading/trailing whitespace
+            numbers = line.split(",")  # Split the line by comma
+            ref_gps[i,0] = numbers[2] #lat
+            ref_gps[i,1] = numbers[3] #lon
+            ref_gps[i,2] = numbers[4]  #rotation
+    with open(query_path,"r") as file:
+        for i in range(query_len):
+            line = file.readline().strip()  # Read the first line and remove leading/trailing whitespace
+            numbers = line.split(",")  # Split the line by comma
+            query_gps[i,0] = numbers[2] #lat
+            query_gps[i,1] = numbers[3] #lon
+            query_gps[i,2] = numbers[4] #rotation
+    
+    #Calculate distance 
+    for row1,frame1 in enumerate(ref_gps):
+        for row2,frame2 in enumerate(query_gps):
+            distance = haversine((ref_gps[row1,0],ref_gps[row1,1]),(query_gps[row2,0],query_gps[row2,1]),unit="m")
+            gps_distance[row1,row2] = distance
+
+    #Plot
+    plt.figure()
+    #Max distance is around 90m
+    plt.title("GPS ground truth for reference run " + str(reference_run) + " and query run " + str(query_run))
+    #cmap: twilight
+    gps_plot = plt.imshow(gps_distance, cmap='viridis', interpolation='nearest')
+    colorbar = plt.colorbar(gps_plot)
+    colorbar.set_label("In meters")
+    plt.xlabel("Query run frame number")
+    plt.ylabel("Reference run frame number")
+    plot_name = "gps_ground_truth for runs " + str(reference_run)+ " and " + str(query_run) + ".png"
+    plt.savefig(plot_name)
+    return gps_distance
+
+def plot_similarity(similarity_run, reference_run, query_run):
+    plt.figure()
+    plt.title("Difference in descriptors between runs " + str(reference_run) + " and " + str(query_run))
+    similarity_plot = plt.imshow(similarity_run, cmap='viridis', interpolation='nearest')
+    colorbar = plt.colorbar(similarity_plot)
+    plt.xlabel("Query run frame number")
+    plt.ylabel("Reference run frame number")
+    plot_name = "Descriptor map for runs " + str(reference_run)+ " and " + str(query_run) + ".png"
+    plt.savefig(plot_name)
+
+#use gps data to find the total translational error
+def rmse_error(gps_ground_truth,max_similarity_idx):
+    pass
---
+++ visual_place_recognition/main.py
@@ -0,0 +1,97 @@
+import glob
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from scipy.spatial.distance import cdist
+import unet_extraction
+from unet_extraction import LearnedFeatureDetector
+import evaluation_tool
+
+#Use the pre-trained weights from many runs -> Assume the provided .pth is from the runs listed as training runs
+gps_paths = [0,1,2,5,6,9,10,11,12,13,14,15,16,17,18,19,25,26,27,28,29,30,31]
+test_paths = [2, 11, 16, 17, 23, 28, 35]
+intersection = [2,11,16,17,28]
+
+def path_processing(run):
+    name = ""
+    if(run < 10):
+        name = "0" + str(run)
+    else:
+        name = str(run)
+    #This is a list
+    images_path = glob.glob("/Volumes/oridatastore09/ThirdPartyData/utias/inthedark/run_0000" + name + "/images/left/*.png")
+    return images_path, len(images_path)
+
+def pre_process_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    tensor = transform(image)
+    return tensor[None,:,:,:]
+
+if __name__ == '__main__':
+    #Set parameters:
+    cuda = True
+    reference_run = 1
+    query_run = 2
+    #Trained for the task of pose estimation
+    network_path = '/home/lamlam/data/networks/network_inthedark_layer32.pth'
+    
+    learned_feature_detector = LearnedFeatureDetector(n_channels=3, 
+                                                      layer_size=32, 
+                                                      window_height=16, 
+                                                      window_width=16, 
+                                                      image_height=384, 
+                                                      image_width=512,
+                                                      checkpoint_path=network_path,
+                                                      cuda=cuda)
+    #Reference path is a list 
+    reference_path, ref_length = path_processing(reference_run)
+    query_path, query_length = path_processing(query_run)
+    transform = transforms.Compose([transforms.ToTensor()])
+    reference_descriptors = []
+    max_similarity_run = np.zeros(query_length,dtype=float)
+    max_similarity_run_index = np.zeros(query_length,dtype=int)
+    similarity_run = np.zeros((ref_length,query_length),dtype=float)
+
+    #Extract keypoints, scores, and descriptors (max pooling) from the reference run and query run
+    #Using this means it's not done in order
+    for frame_path in reference_path:
+        #Normalized. #descriptors has size {1,992,768}. Sparse (with one descriptor for each keypoint)
+        keypoints, descriptors, scores = learned_feature_detector.run(pre_process_image(frame_path))
+        #Use maxpooling
+        descriptors_maxpool = descriptors.squeeze()
+        descriptors_maxpool = F.max_pool1d(descriptors_maxpool, kernel_size=descriptors.shape[-1])
+        descriptors_maxpool = descriptors_maxpool.squeeze(1)
+        reference_descriptors.append(descriptors_maxpool)
+
+    for idx,frame_path in enumerate(query_path):
+        keypoints, descriptors, scores = learned_feature_detector.run(pre_process_image(frame_path))
+        descriptors_maxpool = descriptors.squeeze()
+        descriptors_maxpool = F.max_pool1d(descriptors_maxpool, kernel_size=descriptors.shape[-1])
+        descriptors_maxpool = descriptors_maxpool.squeeze(1)
+        max_similarity = 0
+        max_index = 0
+        for ref_idx,ref_descriptor in enumerate(reference_descriptors):
+            #Use cosine similarity - use the cdist function
+            #similarity = F.cosine_similarity(descriptors_maxpool, ref_descriptor,dim=0)
+            similarity = 1.0-cdist(descriptors_maxpool.reshape(1,-1), ref_descriptor.reshape(1,-1), metric = "cosine")
+            #similarity2 = cdist(descriptors_maxpool.reshape(1,-1), ref_descriptor.reshape(1,-1), metric = "euclidean")
+            #Exactly similar -> similarity = 1. 
+            similarity = float(similarity.astype(float))
+            #if(similarity < 0.98):
+                #similarity = 0.9
+            similarity_run[ref_idx,idx] = similarity
+            if(similarity > max_similarity):
+                max_similarity = similarity
+                max_index = ref_idx
+            max_similarity_run[idx] = max_similarity
+            max_similarity_run_index[idx] = max_index
+
+    #Evaluation
+    gps_distance = evaluation_tool.gps_ground_truth(reference_run,query_run,ref_length,query_length)
+    evaluation_tool.plot_similarity(similarity_run, reference_run, query_run)
+    #evaluation_tool.rmse_error(gps_distance,max_similarity_run_index)
+    
